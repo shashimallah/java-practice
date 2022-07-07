e1db24de2fd67ad395f17dda11841a6a619e8dbe
unknown <shashimallah17@gmail.com>
2022-07-07 19:04:26 +0530

    added SortIntegerArrayByOccurance class.

--- src/com/shashimallah/java8/SortIntegerArrayByOccurance.java
+++ src/com/shashimallah/java8/SortIntegerArrayByOccurance.java
@@ -1,5 +1,21 @@
 package com.shashimallah.java8;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class SortIntegerArrayByOccurance {
+
+    //    input = 1 2 3 4 2 3 2 3 4
+    //    output = 3 2 4 1 / 2 3 4 1
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 2, 3, 2, 3, 4);
+        //        one way
+        Set<Integer> result = list.stream()
+                .collect(Collectors.groupingBy(ele -> ele, Collectors.counting()))
+                .entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new)).keySet();
+        System.out.println(result);
+    }
 
 }
